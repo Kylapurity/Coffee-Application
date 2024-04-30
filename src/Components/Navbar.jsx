3ec7d1ce2a30155b83ec0d89e7c1eba4ee33bab2
pkihiu <p.kihiu@alustudent.com>
2024-04-30 20:51:19 +0200

    update Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Logo from '../assets/Logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-  return (
-    <div>
-      <img src={Logo} alt=""/>
-    </div>
-  )
+    return (
+        <div className='flex justify-between items-center'>
+            <img src={Logo} alt="coffee" className='w-20 h-20 ml-20 pt-5' />
+            <div className='flex justify-around '>
+                <div className=' mr-20' style={{color: '#F8E4BE'}}>
+                    <FontAwesomeIcon icon={faShoppingCart} className='text-xl mr-6 '/>
+                    <FontAwesomeIcon icon={faUser} className='text-xl' />
+                </div>
+                <ul className='flex mr-20'>
+                    <li className='mr-6'>Home</li>
+                    <li className='mr-6'>Menu</li>
+                    <li className='mr-6'>Services</li>
+                    <li className='mr-6'>Products</li>
+                    <li className='mr-6'>Blogs</li>
+                    <li className='mr-6'>Contact</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
+
